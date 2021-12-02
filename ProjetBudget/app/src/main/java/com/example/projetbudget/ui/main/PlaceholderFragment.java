6409ced3abe1cd604916228c3d9fafb0e2d72b8b
NovaCookie <95030874+NovaCookie@users.noreply.github.com>
2021-12-02 13:42:03 +0100

    Update PlaceholderFragment.java

--- ProjetBudget/app/src/main/java/com/example/projetbudget/ui/main/PlaceholderFragment.java
+++ ProjetBudget/app/src/main/java/com/example/projetbudget/ui/main/PlaceholderFragment.java
@@ -4,13 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.projetbudget.R;
 import com.example.projetbudget.databinding.FragmentMainBinding;
@@ -34,32 +30,14 @@ public class PlaceholderFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        pageViewModel = new ViewModelProvider(this).get(PageViewModel.class);
-        int index = 1;
-        if (getArguments() != null) {
-            index = getArguments().getInt(ARG_SECTION_NUMBER);
-        }
-        pageViewModel.setIndex(index);
-    }
-
-    @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
 
-        binding = FragmentMainBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        View rootView = inflater.inflate(R.layout.fragment_main, container ,false);
 
-        final TextView textView = binding.sectionLabel;
-        pageViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+
+        return rootView;
     }
 
     @Override
