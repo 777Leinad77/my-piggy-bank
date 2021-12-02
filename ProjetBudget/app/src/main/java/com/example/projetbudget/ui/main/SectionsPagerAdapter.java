4231b8e22b0431e6f84246d69592b8f0fdf29ed4
NovaCookie <95030874+NovaCookie@users.noreply.github.com>
2021-12-02 14:07:13 +0100

    Update SectionsPagerAdapter.java

--- ProjetBudget/app/src/main/java/com/example/projetbudget/ui/main/SectionsPagerAdapter.java
+++ ProjetBudget/app/src/main/java/com/example/projetbudget/ui/main/SectionsPagerAdapter.java
@@ -7,18 +7,14 @@ import androidx.annotation.StringRes;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
-import com.example.projetbudget.ui.main.PlaceholderFragment;
-import com.example.projetbudget.ui.main.PlaceholderFragment1;
-import com.example.projetbudget.ui.main.PlaceholderFragment2;
+
 import com.example.projetbudget.R;
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @StringRes
     private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2, R.string.tab_text_3};
     private final Context mContext;
-    PlaceholderFragment placeholderFragment;
-    PlaceholderFragment1 placeholderFragment1;
-    PlaceholderFragment2 placeholderFragment2;
+   
     public SectionsPagerAdapter(Context context, FragmentManager fm) {
         super(fm);
         mContext = context;
@@ -30,11 +26,11 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
         if (position == 0) {
-            return placeholderFragment.newInstance(0);
+            return PlaceholderFragment.newInstance(0);
         } else if (position == 1) {
-            return placeholderFragment1.newInstance(1);
+            return PlaceholderFragment1.newInstance(1);
         }
-        return placeholderFragment2.newInstance(2);
+        return PlaceholderFragment2.newInstance(2);
     }
 
     @Nullable
