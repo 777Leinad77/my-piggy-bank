74cf611689b9ebe8be34296a567e85e2694f44f8
patch064 <95026401+patch064@users.noreply.github.com>
2022-03-24 11:29:57 +0100

    pas de problemme

--- ProjetBudget/app/src/main/java/com/example/projetbudget/main1/AjoutDepense.java
+++ ProjetBudget/app/src/main/java/com/example/projetbudget/main1/AjoutDepense.java
@@ -2,10 +2,14 @@ package com.example.projetbudget.main1;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -24,6 +28,11 @@ public class AjoutDepense extends Fragment {
     private AjoutDepenseBinding binding;
     Intent intent;
     Button ad;
+    RadioGroup RG;
+    RadioButton RB;
+    TextView nom;
+    TextView montant;
+
 
     public static AjoutDepense newInstance(int index) {
         AjoutDepense fragment = new AjoutDepense();
@@ -41,11 +50,24 @@ public class AjoutDepense extends Fragment {
         intent = new Intent(getContext(), com.example.projetbudget.activity.MainActivity.class);
         View rootView = inflater.inflate(R.layout.ajout_depense, container, false);
 
+        RG = rootView.findViewById((R.id.RGOpeS));
+
+        nom = rootView.findViewById(R.id.ETNomOpeS);
+        montant = rootView.findViewById(R.id.ETMontantOpeS);
+
         ad = rootView.findViewById(R.id.ad);
         ad.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View c) {
             if (c == ad) {
+
+                int radioId = RG.getCheckedRadioButtonId();
+                RB = rootView.findViewById(radioId);
+
+                String nomS = nom.getText().toString();
+                String montantS = montant.getText().toString();
+
+                Log.i("TestAjoutDepense", "nous avons : " + RB.getText() + " / " + nomS + " / " + " / " + montantS + " .");
                 startActivity(intent);
             }
         }
